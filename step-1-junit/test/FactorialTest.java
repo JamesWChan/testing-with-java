43ea0691fcbf22fde2bb77b9d531103793e58b7b
JamesWChan <jwchan1993@gmail.com>
2017-06-07 11:52:22 +0100

    add second factorial test

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -2,11 +2,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FactorialTest {
+
     @Test
     public void calculate_0() {
         Factorial factorial = new Factorial();
         int testStartValue = 0;
         int expectedResult = 0;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
+
+    @Test
+    public void calculate_1() {
+        Factorial factorial = new Factorial();
+        int testStartValue = 1;
+        int expectedResult = 1;
 
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
