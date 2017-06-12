784b45a51dcb7a152a7a00ef713091cee92572a3
JamesWChan <jwchan1993@gmail.com>
2017-06-12 16:32:04 +0100

    Add files for step 4

--- step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
+++ step-4-selenium/src/test/java/com/mattcalthrop/examples/selenium/Selenium3ExampleIT.java
@@ -18,4 +18,33 @@ public class Selenium3ExampleIT {
 
     private Boolean result;
 
+    @BeforeClass
+    public static void beforeAll() {
+        startUrl = "http://www.google.co.uk/";
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Before
+    public void beforeEach() {
+        driver.get(startUrl);
+    }
+
+    @Test
+    public void checkPageTitleOnInit() {
+        assertEquals("Google", driver.getTitle());
+    }
+
+    @Test
+    public void checkPageTitleAfterSearch(){
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.submit();
+
+    }
 }
